bc849de0c56810b6fd2519a81095526dd4d9e7d3
wtclrsnd <emil42661@gmail.com>
2020-04-25 20:18:03 +0300

    Initial Commit

---
+++ PrettyMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrettyMessage
+//
+//  Created by Emil Shpeklord on 25.04.2020.
+//  Copyright © 2020 Бизнес в стиле .RU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
