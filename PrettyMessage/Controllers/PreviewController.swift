c0bd36b474ce130a857b5a21397c6e894807f783
Grisha1232 <grishaseleznev@yandex.ru>
2020-06-09 17:41:17 +0300

    Preview

---
+++ PrettyMessage/Controllers/PreviewController.swift
@@ -0,0 +1,14 @@
+//
+//  PreviewController.swift
+//  PrettyMessage
+//
+//  Created by Григорий Селезнев on 09.06.2020.
+//  Copyright © 2020 Бизнес в стиле .RU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class preview: UIViewController{
+    
+}
